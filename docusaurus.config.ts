2e488f47936a2a5bc411209be4fa01b47988c7be
Nathy-bajo <bajon7680@gmail.com>
2025-05-26 15:24:39 +0100

    fix github org

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'cyborgnetwork', // Usually your GitHub org/user name.
+  organizationName: 'Cyborg-Network', // Usually your GitHub org/user name.
   projectName: 'Docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
