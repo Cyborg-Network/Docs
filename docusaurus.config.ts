100e7d9b0440552bdd3c0478d5019c7b30c853bd
Tom Bleek <tom.bleek@proton.me>
2025-05-27 19:42:39 +0200

    Trailing slash false

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.cyborgnetwork.io',
+  url: 'https://docs.cyborgnetwork.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'cyborgnetwork', // Usually your GitHub org/user name.
   projectName: 'Docs', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
