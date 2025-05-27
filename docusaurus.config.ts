204ad086f11cc1ca0005dc28e0b6cd294e705ca9
Tom Bleek <tom.bleek@proton.me>
2025-05-27 19:53:45 +0200

    Add CNAME

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://Cyborg-Network.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Docs',
+  baseUrl: '/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
